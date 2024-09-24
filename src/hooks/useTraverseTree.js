049aed6b16da3fd8e805dd2ad17c5201d0c3b366
Besal Raj <48745482+besalraj30@users.noreply.github.com>
2024-09-25 00:04:09 +0530

    Comment: Update useTraverseTree.js

--- src/hooks/useTraverseTree.js
+++ src/hooks/useTraverseTree.js
@@ -1,6 +1,9 @@
 const useTraverseTree = () => {
     function insertNode(tree, folderId, item, isFolder) {
         if(tree.id === folderId && tree.isFolder) {
+
+            //opposite of shit, adds element as first element in array
+            
             tree.items.unshift({
                 id: new Date().getTime(),
                 name: item,
